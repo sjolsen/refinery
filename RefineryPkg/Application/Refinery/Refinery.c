4511db2e06651e78aaa38e890a4f67e9f265ed84
Stuart Olsen <stuart@sj-olsen.com>
2024-01-25 02:42:07 -0600

    Run UncrustifyCheck.

--- RefineryPkg/Application/Refinery/Refinery.c
+++ RefineryPkg/Application/Refinery/Refinery.c
@@ -2,7 +2,11 @@
 #include <Library/UefiLib.h>
 
 EFI_STATUS EFIAPI
-RefineryMain(IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable) {
-    Print(L"RefineryMain\r\n");
-    return EFI_SUCCESS;
+RefineryMain (
+  IN EFI_HANDLE        ImageHandle,
+  IN EFI_SYSTEM_TABLE  *SystemTable
+  )
+{
+  Print (L"RefineryMain\r\n");
+  return EFI_SUCCESS;
 }
