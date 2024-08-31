5f59d8f5e06031e2e5d134301f9c981c1a1fc098
Stuart Olsen <stuart@sj-olsen.com>
2024-08-31 14:23:47 -0500

    Delete collect-suite

--- BoraxPkg/Lisp/BoraxVirtualMachine/image-test.lisp
+++ BoraxPkg/Lisp/BoraxVirtualMachine/image-test.lisp
@@ -1,7 +1,6 @@
 (uiop:define-package :borax-virtual-machine/image-test
   (:use :uiop/common-lisp :clunit :cl-locatives
-        :borax-virtual-machine/image
-        :borax-virtual-machine/mock-record))
+        :borax-virtual-machine/image))
 
 (in-package :borax-virtual-machine/image-test)
 
@@ -19,70 +18,6 @@
   (assert-equal #x3fffffffffffffff (borax-vm/cl:most-positive-fixnum +64-bit+))
   (assert-equal #x-4000000000000000 (borax-vm/cl:most-negative-fixnum +64-bit+)))
 
-(defsuite collect-suite ())
-
-(deftest test-cons-discard (collect-suite)
-  (with-image nil
-    (borax-vm/cl:cons 1 2)
-    (reify-image)
-    (assert-equal 0 (length (objects *image*)))))
-
-(deftest test-cons-keep (collect-suite)
-  (with-image nil
-    (setf (root *image*) (borax-vm/cl:cons 1 2))
-    (reify-image)
-    (assert-equal 1 (length (objects *image*)))))
-
-(deftest test-cons-keep-some (collect-suite)
-  (with-image nil
-    (with-slots (root) *image*
-      (dotimes (n 5)
-        (borax-vm/cl:cons 1 2)
-        (push (borax-vm/cl:cons 3 4) root))
-      (reify-image))
-    (assert-equal 10 (length (objects *image*)))
-    (loop for i upfrom 0
-          for object across (objects *image*)
-          do (assert-equal i (index object)))))
-
-(defun make-circular (n)
-  (let* ((last (borax-vm/cl:cons 0 +unbound+))
-         (first last))
-    (dotimes (i n)
-      (setf first (borax-vm/cl:cons (1+ i) first)))
-    (setf (borax-vm/cl:cdr last) first)))
-
-(deftest test-circular-discard (collect-suite)
-  (with-image nil
-    (make-circular 5)
-    (reify-image)
-    (assert-equal 0 (length (objects *image*)))))
-
-(deftest test-circular-keep (collect-suite)
-  (with-image nil
-    (setf (root *image*) (make-circular 5))
-    (reify-image)
-    (assert-equal 6 (length (objects *image*)))))
-
-(defun make-funny-record ()
-  (let ((class (make-word-record +unbound+ #())))
-    (setf (record-class class) class)
-    (let* ((nested (make-word-record class #(1 2 3)))
-           (data (make-array 3 :initial-contents (list nested nested +unbound+))))
-      (make-object-record class data))))
-
-(deftest test-record-discard (collect-suite)
-  (with-image nil
-    (make-funny-record)
-    (reify-image)
-    (assert-equal 0 (length (objects *image*)))))
-
-(deftest test-record-keep (collect-suite)
-  (with-image nil
-    (setf (root *image*) (make-funny-record))
-    (reify-image)
-    (assert-equal 3 (length (objects *image*)))))
-
 (defsuite record-object-suite ())
 
 (defclass test ()
@@ -94,8 +29,8 @@
 (deftest test-basic-object (record-object-suite)
   (with-image nil
     (let ((o (make-instance 'test)))
-      (assert-equal (list (find-class 'test) 42 t "hello")
-          (mapcar #'dereference (sub-objects o))))))
+      (assert-equal '(42 t "hello")
+          (mapcar #'dereference (cdr (sub-objects o)))))))
 
 (defclass test2 (test)
   ((a :initform #\A))
@@ -104,5 +39,5 @@
 (deftest test-derived-object (record-object-suite)
   (with-image nil
     (let ((o (make-instance 'test2)))
-      (assert-equal (list (find-class 'test2) 42 t "hello" #\A)
-          (mapcar #'dereference (sub-objects o))))))
+      (assert-equal '(42 t "hello" #\A)
+          (mapcar #'dereference (cdr (sub-objects o)))))))
