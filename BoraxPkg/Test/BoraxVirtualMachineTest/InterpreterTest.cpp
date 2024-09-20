280642e1def214b5d68010dc8560eb669d4bf95c
Stuart Olsen <stuart@sj-olsen.com>
2024-09-19 21:26:08 -0500

    Implement basic test fixture for interpreter testing

---
+++ BoraxPkg/Test/BoraxVirtualMachineTest/InterpreterTest.cpp
@@ -0,0 +1,80 @@
+#include "MemoryTest.hpp"
+
+extern "C" {
+  #include <Library/BoraxInterpreter.h>
+  #include <Library/BoraxObjectFile.h>
+}
+
+#include "BoraxVirtualMachineTest.hpp"
+#include "MockEvent.hpp"
+#include "MockFile.hpp"
+
+struct InterpreterDeleter {
+  void
+  operator() (
+    BORAX_INTERPRETER  *Interp
+    )
+  {
+    BoraxInterpreterCleanup (Interp);
+  }
+};
+
+using AutoInterpreter = std::unique_ptr<BORAX_INTERPRETER, InterpreterDeleter>;
+
+class InterpreterInitError : public std::exception {
+};
+
+class InterpreterTests : public MemoryTests {
+public:
+  MockEventEngine EventEngine;
+  AutoInterpreter Interp;
+
+  AutoPin
+  LoadImage (
+    )
+  {
+    EFI_STATUS  Status;
+    PosixFile   File { TestBase / "InitialImage.bxo" };
+    BORAX_PIN   *Pin;
+
+    Status = BoraxLoadObjectFile (&Alloc, File.GetProtocol (), &Pin);
+    if (EFI_ERROR (Status)) {
+      throw InterpreterInitError { };
+    }
+
+    return AutoPin { Pin };
+  }
+
+  void
+  SetUp (
+    ) override
+  {
+    EFI_STATUS         Status;
+    BORAX_INTERPRETER  *Interp;
+    AutoPin            Pin;
+
+    MemoryTests::SetUp ();
+
+    Pin = LoadImage ();
+
+    Status = BoraxInterpreterInit (&Alloc, Pin->Object, &Interp);
+    if (EFI_ERROR (Status)) {
+      throw InterpreterInitError { };
+    }
+
+    this->Interp = AutoInterpreter { Interp };
+  }
+
+  void
+  TearDown (
+    ) override
+  {
+    this->Interp.reset ();
+
+    MemoryTests::TearDown ();
+  }
+};
+
+TEST_F (InterpreterTests, NullTest) {
+  // Just make sure setup works
+}
